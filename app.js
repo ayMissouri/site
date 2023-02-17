a2c80f6a647fe8f02f5c80721c49b924da615bc3
Ahmed Missouri <ahmedmissouri@icloud.com>
2023-02-17 17:13:21 +0000

    updated links

--- app.js
+++ app.js
@@ -33,13 +33,6 @@ const enhance = (id) => {
   });
 };
 
-window.transitionToPage = function (href) {
-  document.querySelector("body").style.opacity = 0;
-  setTimeout(function () {
-    window.location.href = href;
-  }, 500);
-};
-
 document.addEventListener("DOMContentLoaded", function (event) {
   document.querySelector("body").style.opacity = 1;
 });
