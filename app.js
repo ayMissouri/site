fd5f4289d47282fd77871ed38295ed693aaf05ef
Ahmed Missouri <ahmedmissouri@icloud.com>
2023-02-17 01:28:01 +0000

    formatting

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
 const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const enhance = id => {
+const enhance = (id) => {
   const element = document.getElementById(id),
-        text = element.innerText.split("");
-  
+    text = element.innerText.split("");
+
   element.innerText = "";
-  
+
   text.forEach((value, index) => {
     const outer = document.createElement("span");
-    
+
     outer.className = "outer";
-    
+
     const inner = document.createElement("span");
-    
+
     inner.className = "inner";
-    
+
     inner.style.animationDelay = `${rand(-5000, 0)}ms`;
-    
+
     const letter = document.createElement("span");
-    
+
     letter.className = "letter";
-    
+
     letter.innerText = value;
-    
-    letter.style.animationDelay = `${index * 1000 }ms`;
-    
-    inner.appendChild(letter);    
-    
-    outer.appendChild(inner);    
-    
+
+    letter.style.animationDelay = `${index * 1000}ms`;
+
+    inner.appendChild(letter);
+
+    outer.appendChild(inner);
+
     element.appendChild(outer);
   });
-}
+};
 
-window.transitionToPage = function(href) {
-    document.querySelector('body').style.opacity = 0
-    setTimeout(function() { 
-        window.location.href = href
-    }, 500)
-}
+window.transitionToPage = function (href) {
+  document.querySelector("body").style.opacity = 0;
+  setTimeout(function () {
+    window.location.href = href;
+  }, 500);
+};
 
-document.addEventListener('DOMContentLoaded', function(event) {
-    document.querySelector('body').style.opacity = 1
-})
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.querySelector("body").style.opacity = 1;
+});
 
 enhance("insta");
 enhance("disc");
@@ -52,38 +52,38 @@ const trailer = document.getElementById("trailer");
 
 const animateTrailer = (e, interacting) => {
   const x = e.clientX - trailer.offsetWidth / 2,
-        y = e.clientY - trailer.offsetHeight / 2;
-  
-  const keyframes = {
-    transform: `translate(${x}px, ${y}px) scale(${interacting ? 1.75 : 1})`
-  }
-  
-  trailer.animate(keyframes, { 
-    duration: 300, 
-    fill: "forwards" 
-  });
-}
+    y = e.clientY - trailer.offsetHeight / 2;
 
-const getTrailerClass = type => {
-  switch(type) {
+  const keyframes = {
+    transform: `translate(${x}px, ${y}px) scale(${interacting ? 1.75 : 1})`,
+  };
+
+  trailer.animate(keyframes, {
+    duration: 300,
+    fill: "forwards",
+  });
+};
+
+const getTrailerClass = (type) => {
+  switch (type) {
     case "video":
       return "fa-solid fa-play";
     default:
-      return "fa-solid fa-arrow-up-right"; 
+      return "fa-solid fa-arrow-up-right";
   }
-}
+};
 
-window.onmousemove = e => {
+window.onmousemove = (e) => {
   const interactable = e.target.closest(".interactable"),
-        interacting = interactable !== null;
-  
+    interacting = interactable !== null;
+
   const icon = document.getElementById("trailer-icon");
-  
+
   animateTrailer(e, interacting);
-  
+
   trailer.dataset.type = interacting ? interactable.dataset.type : "";
-  
-  if(interacting) {
+
+  if (interacting) {
     icon.className = getTrailerClass(interactable.dataset.type);
   }
-}
+};
