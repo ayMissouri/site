9babfb26243ce1a9b9c027afeac9b59ea6ce6934
Ahmed Missouri <ahmedmissouri@icloud.com>
2023-02-17 17:06:21 +0000

    new background who dis

--- app.js
+++ app.js
@@ -87,3 +87,10 @@ window.onmousemove = (e) => {
     icon.className = getTrailerClass(interactable.dataset.type);
   }
 };
+
+document.body.addEventListener("pointermove", (e) => {
+  const { currentTarget: el, clientX: x, clientY: y } = e;
+  const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
+  el.style.setProperty("--posX", x - l - w / 2);
+  el.style.setProperty("--posY", y - t - h / 2);
+});
